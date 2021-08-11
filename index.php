808f5e7b40cee9607572f6d0410030e5b6c5df9e
James Pederson <james@jpederson.com>
2021-08-11 00:18:29 -0500

    Adjust quality so that the thumbnails are much smaller than the originals.

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ function make_thumb( $src, $dest, $desired_width ) {
 	imagecopyresampled( $virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height );
 	
 	/* create the physical thumbnail image to its destination */
-	imagejpeg( $virtual_image, $dest, 100 );
+	imagejpeg( $virtual_image, $dest, 60 );
 
 }
 
